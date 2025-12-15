6f2a8b1c1dec7e45052ff546caf128f09b240f52
daniilok21 <sergeevdaniil383@gmail.com>
2025-12-15 21:56:11 +0300

    пж заработай

--- core/src/main/java/io/github/some_example_name/GameSettings.java
+++ core/src/main/java/io/github/some_example_name/GameSettings.java
@@ -3,6 +3,16 @@ package io.github.some_example_name;
 public class GameSettings {
     public static final int SCREEN_WIDTH = 1280;
     public static final int SCREEN_HEIGHT = 720;
+
+    // Игровые константы
+    public static final int CUBE_SIZE = 50;
+    public static final int MOVE_SPEED = 5;
+
+    // Сетевые настройки
+    public static final int PORT = 9090;
+    public static final int FRAME_RATE = 60;
+    public static final int FRAME_DELAY_MS = 1000 / FRAME_RATE;
+
     // физика
     public static final float STEP_TIME = 1f / 60f;
     public static final int VELOCITY_ITERATIONS = 6;
--- core/src/main/java/io/github/some_example_name/MyGdxGame.java
+++ core/src/main/java/io/github/some_example_name/MyGdxGame.java
@@ -1,99 +1,85 @@
 package io.github.some_example_name;
 
-import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.*;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import io.github.some_example_name.game.GameState;
-import io.github.some_example_name.game.PlayerInput;
-import io.github.some_example_name.net.Client;
-import io.github.some_example_name.net.Server;
+import io.github.some_example_name.managers.AudioManager;
+import io.github.some_example_name.screens.GameScreen;
+import io.github.some_example_name.screens.MenuScreen;
 
-public class MyGdxGame extends ApplicationAdapter {
-    SpriteBatch batch;
-    ShapeRenderer shapeRenderer;
-    boolean isHost = true;
-    String hostIp = "192.168.0.15";
+import static io.github.some_example_name.GameSettings.*;
 
-    Server server;
-    Client client;
-    GameState localState;
+public class MyGdxGame extends Game {
+    public SpriteBatch batch;
+    public OrthographicCamera camera;
+    public BitmapFont font;
 
+    public AudioManager audioManager;
+
+    // Экранs
+    public MenuScreen menuScreen;
+    public GameScreen gameScreen;
+
+
+    public boolean isHost = true;
+    public String hostIp = "192.168.0.15";
+    public int port = 9090;
 
     @Override
     public void create() {
+        // Инициализация графики
         batch = new SpriteBatch();
-        shapeRenderer = new ShapeRenderer();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-        if (isHost) {
-            server = new Server();
-            server.start(9090);
-            localState = server.getLocalState();
-        }
-        else {
-            client = new Client();
-            if (!client.connect(hostIp, 9090)) {
-                Gdx.app.exit();
-            }
-            localState = new GameState();
-        }
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+
+        audioManager = new AudioManager();
+
+        menuScreen = new MenuScreen(this);
+        gameScreen = new GameScreen(this);
+
+        setScreen(menuScreen);
     }
 
     @Override
     public void render() {
-        // Обновление состояния
-        if (!isHost) {
-            localState = client.getState();
-        } else {
-            localState = server.getLocalState();
-        }
-
-        // Обработка ввода
-        if (Gdx.input.isTouched()) {
-            if (isHost) {
-                server.moveServerCubeRight();
-            }
-            else {
-                PlayerInput input = new PlayerInput();
-                input.moveRight = true;
-                client.sendInput(input);
-            }
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            if (isHost) {
-                server.moveServerCubeLeft();
-            }
-            else {
-                PlayerInput input = new PlayerInput();
-                input.moveLeft = true;
-                client.sendInput(input);
-            }
-        }
-
-        Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-
-        // хост
-        shapeRenderer.setColor(Color.RED);
-        shapeRenderer.rect(localState.serverCubeX, localState.serverCubeY, 50, 50);
-
-        // клиент
-        shapeRenderer.setColor(Color.BLUE);
-        shapeRenderer.rect(localState.clientCubeX, localState.clientCubeY, 50, 50);
-
-        shapeRenderer.end();
-
+        super.render();
     }
 
     @Override
     public void dispose() {
-        if (server != null) server.stop();
-        if (client != null) client.disconnect();
         batch.dispose();
-        shapeRenderer.dispose();
+        font.dispose();
+
+        if (menuScreen != null) {
+            menuScreen.dispose();
+        }
+
+        if (gameScreen != null) {
+            gameScreen.dispose();
+        }
+    }
+
+    public void showGameScreen(boolean asHost, String ip) {
+        this.isHost = asHost;
+        if (ip != null && !ip.isEmpty()) {
+            this.hostIp = ip;
+        }
+
+        gameScreen.initializeNetwork();
+        setScreen(gameScreen);
+    }
+
+    public void showMenuScreen() {
+        if (gameScreen != null) {
+            gameScreen.disconnect();
+        }
+        setScreen(menuScreen);
     }
 }
