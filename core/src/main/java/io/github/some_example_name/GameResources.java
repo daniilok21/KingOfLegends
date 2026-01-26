d829835019caa47825a0ed52afe9ad8372cbdd3e
draklays666 <sasaykonstantinov5@gmail.com>
2026-01-26 18:38:12 +0300

    норм джоин по айпи

--- core/src/main/java/io/github/some_example_name/GameResources.java
+++ core/src/main/java/io/github/some_example_name/GameResources.java
@@ -14,6 +14,21 @@ public class GameResources {
     public static final String RED_PLAYER_SPRITE_SHEET = "textures/red_player.png";
     public static final String BLUE_PLAYER_SPRITE_SHEET = "textures/blue_player.png";
 
+    public static final String BLUE_PLAYER_IDLE_SHEET = "textures/players/blue/Idle.png";
+    public static final String BLUE_PLAYER_RUN_SHEET = "textures/players/blue/Run.png";
+    public static final String BLUE_PLAYER_JUMP_SHEET = "textures/players/blue/Jump.png";
+    public static final String BLUE_PLAYER_ATTACK_SHEET = "textures/players/blue/Attack 1.png";
+    public static final String BLUE_PLAYER_DODGE_SHEET = "textures/players/blue/Defend.png";
+    public static final String BLUE_PLAYER_HIT_SHEET = "textures/players/blue/Hurt.png";
+    public static final String RED_PLAYER_IDLE_SHEET = "textures/players/red/Idle.png";
+    public static final String RED_PLAYER_RUN_SHEET = "textures/players/red/Run.png";
+    public static final String RED_PLAYER_JUMP_SHEET = "textures/players/red/Jump.png";
+    public static final String RED_PLAYER_ATTACK_SHEET = "textures/players/red/Attack 1.png";
+    public static final String RED_PLAYER_DODGE_SHEET = "textures/players/red/Defend.png";
+    public static final String RED_PLAYER_HIT_SHEET = "textures/players/red/Hurt.png";
+
+
+
     // Джойстик
     public static final String JOYSTICK_BG = "textures/Joystick.png";
     public static final String JOYSTICK_HANDLE = "textures/LargeHandleFilled.png";
--- core/src/main/java/io/github/some_example_name/GameSettings.java
+++ core/src/main/java/io/github/some_example_name/GameSettings.java
@@ -7,9 +7,9 @@ public class GameSettings {
 
     // Настройки игрока
     public static final int PLAYER_WIDTH = 64;
-    public static final int PLAYER_HEIGHT = 64;
+    public static final int PLAYER_HEIGHT = 100;
     public static final float PLAYER_MOVE_FORCE = 500f;
-    public static final float PLAYER_JUMP_FORCE = 35f;
+    public static final float PLAYER_JUMP_FORCE = 60f;
     public static final float PLAYER_MAX_VELOCITY = 20f;
     public static final float PLAYER_MAX_KNOCKBACK_VELOCITY = 60f;
     public static final float HIT_STUN_DURATION = 0.3f;
